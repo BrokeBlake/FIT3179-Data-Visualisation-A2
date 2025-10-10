d540060e9a7c1569bf996599fbc96c3a10e203a8
BrokeBlake <btra0031@student.monash.edu>
2025-10-10 15:42:30 +1100

    Html updated
    Downscaled & Rounded lat lon values for point map

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,2 +1,5 @@
-var vg_1 = "test.json";
-vegaEmbed("#map", vg_1).then(function(result){}).catch(console.error);
+var vg_1 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/test.json";
+vegaEmbed("#point_map", vg_1).then(function(result){}).catch(console.error);
+
+var vg_1 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/chloropleth_suburbs.json";
+vegaEmbed("#chloropleth_map", vg_1).then(function(result){}).catch(console.error);
