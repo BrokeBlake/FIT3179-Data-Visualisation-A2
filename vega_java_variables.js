f110c3aacc5589a786188956de8a64eb38080cb7
BrokeBlake <btra0031@student.monash.edu>
2025-10-18 19:57:47 +1100

    Fixed Widths

--- vega_java_variables.js
+++ vega_java_variables.js
@@ -4,8 +4,14 @@ vegaEmbed(
 
 ).then(function(result){}).catch(console.error);
 
-var vg_1 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/chloropleth_suburbs.json";
+var vg_2 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/chloropleth_suburbs.json";
 vegaEmbed(
-    "#chloropleth_map", vg_1
+    "#chloropleth_map", vg_2
+
+).then(function(result){}).catch(console.error);
+
+var vg_3 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/density_area_chart.json";
+vegaEmbed(
+    "#area_chart", vg_3
 
 ).then(function(result){}).catch(console.error);
