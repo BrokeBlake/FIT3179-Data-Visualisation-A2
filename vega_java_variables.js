0730f4a123713291e5c14f5fd8967a5c39093210
BrokeBlake <btra0031@student.monash.edu>
2025-10-22 12:45:13 +1100

    Updated java transparent background, added transparent stroke

--- vega_java_variables.js
+++ vega_java_variables.js
@@ -1,23 +1,27 @@
 var vg_1 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/test.json";
-vegaEmbed(
-    "#point_map", vg_1
-
-).then(function(result){}).catch(console.error);
-
 var vg_2 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/chloropleth_suburbs.json";
-vegaEmbed(
-    "#chloropleth_map", vg_2
-
-).then(function(result){}).catch(console.error);
-
 var vg_3 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/density_area_chart.json";
+var vg_4 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/city_densities.json";
+
 vegaEmbed(
-    "#area_chart", vg_3
+    "#point_map", vg_1, { renderer: 'svg' }
 
 ).then(function(result){}).catch(console.error);
 
-var vg_4 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/city_densities.json";
+
 vegaEmbed(
-    "#scatter_plot", vg_4
+    "#chloropleth_map", vg_2, { renderer: 'svg' }
+
+).then(function(result){}).catch(console.error);
+
+
+vegaEmbed(
+    "#area_chart", vg_3, { renderer: 'svg' }
+
+).then(function(result){}).catch(console.error);
+
+
+vegaEmbed(
+    "#scatter_plot", vg_4, { renderer: 'svg' }
 
 ).then(function(result){}).catch(console.error);
