fd750fea4f1fdf24dfca409b38f2d0851e7d32b1
BrokeBlake <btra0031@student.monash.edu>
2025-10-25 12:39:33 +1100

    Fixed data again
    Nearly finished horizontal bar chart

--- vega_java_variables.js
+++ vega_java_variables.js
@@ -1,7 +1,7 @@
 var vg_1 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/test.json";
 var vg_2 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/chloropleth_suburbs.json";
 var vg_3 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/density_area_chart.json";
-var vg_4 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/city_densities.json";
+var vg_4 = "https://raw.githubusercontent.com/BrokeBlake/FIT3179-Data-Visualisation-A2/main/vega-lite/suburb_change.json";
 
 vegaEmbed(
     "#point_map", vg_1, { renderer: 'svg' }
